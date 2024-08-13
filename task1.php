afcbbdf05754f6de7429164f50b5707ef14c18df
maedeh <m.goharshahi3@gmail.com>
2024-08-13 10:54:57 +0330

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,12 @@
+<?php
+$Info = array(["fname"=>"mahdi","lname"=>"kardani","age"=>20,"gender"=>true],
+["fname"=>"aref","lname"=>"askari","age"=>16,"gender"=>true],
+["fname"=>"mahya","lname"=>"hamidi","age"=>16,"gender"=>false],
+["fname"=>"maedeh","lname"=>"goharshahi","age"=>20,"gender"=>false],
+["fname"=>"haniyeh","lname"=>"zarif","age"=>24,"gender"=>false],
+["fname"=>"reyhaneh","lname"=>"soltani","age"=>16,"gender"=>false],
+["fname"=>"zeinab","lname"=>"gharib","age"=>17,"gender"=>false]);
+
+
+
+?>
